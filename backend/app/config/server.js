27d229c02301198fcc7e1e4cd1d04a087f8c3492
Ng Wing Chung <nwc27101994@yahoo.com.hk>
2021-02-11 22:39:02 +0800

    first commit

---
+++ backend/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4567ac0b6831440c62c85dd4583c6901'),
+    },
+  },
+});
---
+++ backend/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e750cf0b-8d9e-4e28-b984-622b6f590542'
+};
